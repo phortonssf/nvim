d210dd07544786b5b07fdb106179ac42e15a4452
Peter Horton <phorton@softstackfactory.com>
2021-10-17 22:24:34 -0700

    (feat) autosession

---
+++ lua/digitaldive/auto-session.lua
@@ -0,0 +1,4 @@
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
+require('auto-session').setup {
+    auto_session_enable_last_session=true,
+}
