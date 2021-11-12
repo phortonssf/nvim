854f0b96ced71ce075936bcb5f3b2e4a32f77f04
Peter Horton <phorton@softstackfactory.com>
2021-11-12 15:53:46 -0800

    (fix) removes lsp ctrl-k keybind

--- init.lua
+++ init.lua
@@ -161,7 +161,11 @@ require('gitsigns').setup {
   },
 }
 
-require("bufferline").setup{}
+require("bufferline").setup{
+ options = {
+   numbers = "ordinal"
+  } 
+}
 -- Telescope
 require('telescope').setup {
   defaults = {
@@ -169,12 +173,12 @@ require('telescope').setup {
       i = {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
-      },
+},
     },
   },
 }
 --Add leader shortcuts
-vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+ vim.api.nvim_set_keymap('n', '<leader>ss', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
