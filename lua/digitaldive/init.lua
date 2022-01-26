ff421dc000ba3b34c68a08b79cd8710ddbd5480b
Peter Horton <phorton@softstackfactory.com>
2022-01-26 15:18:12 -0800

    (feat:nvim) remove uneeded core from init.lua q Please enter the commit message for your changes. Lines starting

--- lua/digitaldive/init.lua
+++ lua/digitaldive/init.lua
@@ -1,9 +1,16 @@
+-- require("digitaldive.lua-line")
+-- require("digitaldive.telescope")
+-- require("digitaldive.treesitter")
+-- require("digitaldive.bufferline")
+-- require("digitaldive.specs")
+-- -- require("digitaldive.auto-session")
+-- require("digitaldive.lsp")
 -- Install packer
-require("digitaldive.packer")
 -- disable unused plugins
 require('digitaldive.disable-builtins')
+require("digitaldive.packer")
 local lua_config_dir = vim.fn.stdpath('config') .. '/lua'
-local config_dirs = {'core','lsp', 'plugins'}
+local config_dirs = {'lsp','plugins', 'core'}
 
 local function source_files_from_dir(directory)
   for _, file in pairs(vim.fn.readdir(directory)) do
@@ -12,4 +19,7 @@ local function source_files_from_dir(directory)
       vim.fn.execute('source ' .. file)
     end
   end
+end
+for _, dir in pairs(config_dirs) do
+  source_files_from_dir(lua_config_dir .. '/digitaldive/' .. dir)
 end
