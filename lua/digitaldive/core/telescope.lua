8dee3750e6df2634673ae71004c0bf76771062e9
Peter Horton <phorton@softstackfactory.com>
2022-12-15 20:57:38 -0800

    (feat:nvim) adds command palette exention telescope

--- lua/digitaldive/core/telescope.lua
+++ lua/digitaldive/core/telescope.lua
@@ -11,10 +11,62 @@ require('telescope').setup {
         ['<C-d>'] = false,
         ["<c-t>"] = trouble.open_with_trouble
        },
-      n = {["<c-t>"] = trouble.open_with_trouble }
+      n = {
+        ["<c-t>"] = trouble.open_with_trouble,
+        ["<c-c>"] = require('telescope.actions').close
+      }
     },
   },
   extensions = {
+    project = {
+      base_dirs = {
+        {path = '~/.dotfiles', max_depth = 5},
+        {path = '~/projects', max_depth = 5},
+      },
+      hidden_files = true, -- default: false
+      theme = "dropdown",
+      order_by = "asc",
+      sync_with_nvim_tree = true, -- default false
+    },
+     command_palette = {
+      {"File",
+        { "entire selection (C-a)", ':call feedkeys("GVgg")' },
+        { "save current file (C-s)", ':w' },
+        { "save all files (C-A-s)", ':wa' },
+        { "quit (C-q)", ':qa' },
+        { "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
+        { "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
+        { "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
+        { "files (C-f)",     ":lua require('telescope.builtin').find_files()", 1 },
+      },
+      {"Help",
+        { "tips", ":help tips" },
+        { "cheatsheet", ":help index" },
+        { "tutorial", ":help tutor" },
+        { "summary", ":help summary" },
+        { "quick reference", ":help quickref" },
+        { "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
+      },
+      {"Vim",
+        { "reload vimrc", ":source $MYVIMRC" },
+        { 'check health', ":checkhealth" },
+        { "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
+        { "commands", ":lua require('telescope.builtin').commands()" },
+        { "command history", ":lua require('telescope.builtin').command_history()" },
+        { "registers (A-e)", ":lua require('telescope.builtin').registers()" },
+        { "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
+        { "vim options", ":lua require('telescope.builtin').vim_options()" },
+        { "keymaps", ":lua require('telescope.builtin').keymaps()" },
+        { "buffers", ":Telescope buffers" },
+        { "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
+        { "paste mode", ':set paste!' },
+        { 'cursor line', ':set cursorline!' },
+        { 'cursor column', ':set cursorcolumn!' },
+        { "spell checker", ':set spell!' },
+        { "relative number", ':set relativenumber!' },
+        { "search highlighting (F12)", ':set hlsearch!' },
+      }
+    },
     file_browser = {
       hijack_netrw = true,
       hidden = true,
@@ -32,12 +84,14 @@ require('telescope').setup {
     },
   },
 }
-
+require'telescope'.load_extension('project')
+require('telescope').load_extension('command_palette')
 require("telescope").load_extension "file_browser"
 vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 local previewers = require('telescope.previewers')
 local builtin = require('telescope.builtin')
 local M = {}
+
 
 local delta = previewers.new_termopen_previewer {
   get_command = function(entry)
