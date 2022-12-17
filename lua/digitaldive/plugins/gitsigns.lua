8d5e55afc207a22ed97039ddfb8896c3baf8ce06
Peter Horton <phorton@softstackfactory.com>
2022-12-16 18:24:07 -0800

    (fix:nvim) moved core to after/plugins

--- lua/digitaldive/plugins/gitsigns.lua
+++ lua/digitaldive/plugins/gitsigns.lua
@@ -12,6 +12,10 @@ require('gitsigns').setup {
     noremap = true,
 
     ['n <leader>nh'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'"},
-    ['n <leader>ph'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
+    ['n <leader>hn'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
+
+    ['n [c'] = "<cmd>Gitsigns prev_hunk<CR>",
+    ['n ]c'] = "<cmd>Gitsigns prev_hunk<CR>",
+    ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>; <cmd>echo "staged hunk"'
     }
 }
