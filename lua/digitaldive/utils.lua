33cd16434017ff3e9964055e78947240f6d53474
Peter Horton <phorton@softstackfactory.com>
2022-12-16 19:33:15 -0800

    (feat:nvim) adds utils for keymaps

---
+++ lua/digitaldive/utils.lua
@@ -0,0 +1,16 @@
+
+local M = {}
+  function M.map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+    -- function M.map(mode, lhs, rhs, opts)
+    -- local options = { noremap = true }
+    -- if opts then
+    --     options = vim.tbl_extend("force", options, opts)
+    -- end
+    -- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+-- end
+
+return M
