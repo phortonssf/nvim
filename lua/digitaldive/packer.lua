e96aacc08257078b2159ee7d1bbe1268a0fd9cd5
Peter Horton <phorton@softstackfactory.com>
2022-12-16 19:32:46 -0800

    (feat:nvim) add themese fixes formatting

--- lua/digitaldive/packer.lua
+++ lua/digitaldive/packer.lua
@@ -6,8 +6,8 @@ end
 
 vim.cmd [[
   augroup Packer
-    autocmd!
-    autocmd BufWritePost init.lua PackerCompile
+  autocmd!
+  autocmd BufWritePost init.lua PackerCompile
   augroup end
 ]]
 
@@ -55,96 +55,116 @@ require("packer").startup({
     use 'folke/lsp-colors.nvim'
 
     -- Language Plugins
-   use {
-    "cuducos/yaml.nvim",
-    ft = {"yaml"}, -- optional
-    requires = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim" -- optional
-    },
-    config = function ()
-      require("yaml_nvim").init()
-    end,
-  }
-  -- docker file syntax
-  use 'ekalinin/Dockerfile.vim'
-  -- markdown preview
-  use {"ellisonleao/glow.nvim"}
+    use {
+      "cuducos/yaml.nvim",
+      ft = {"yaml"}, -- optional
+      requires = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim" -- optional
+      },
+      config = function ()
+        require("yaml_nvim").init()
+      end,
+    }
+    -- docker file syntax
+    use 'ekalinin/Dockerfile.vim'
+    -- markdown preview
+    use {"ellisonleao/glow.nvim"}
     -- terraform completion
-  use 'juliosueiras/vim-terraform-completion'
-  use 'hashivim/vim-terraform'
-  use 'vim-syntastic/syntastic'
-  use 'neomake/neomake'
+    use 'juliosueiras/vim-terraform-completion'
+    use 'hashivim/vim-terraform'
+    use 'vim-syntastic/syntastic'
+    use 'neomake/neomake'
 
-  -- GIT
-  -- -- Add git related info in the signs columns and popups
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
-  use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-  -- use 'ruanyl/vim-gh-line' -- open current line in github/
-  use 'f-person/git-blame.nvim'
-  use { "LinArcX/telescope-command-palette.nvim" }
-  use 'nvim-lua/plenary.nvim'
+    -- GIT
+    -- -- Add git related info in the signs columns and popups
+    use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+    use 'tpope/vim-fugitive' -- Git commands in nvim
+    use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+    use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
+    -- use 'ruanyl/vim-gh-line' -- open current line in github/
+    use 'f-person/git-blame.nvim'
+    use { "LinArcX/telescope-command-palette.nvim" }
+    use 'nvim-lua/plenary.nvim'
 
- -- Highlight, edit, and navigate code using a fast incremental parsing library
-  use 'nvim-treesitter/nvim-treesitter'
-  -- Additional textobjects for treesitter
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
-  use 'nvim-treesitter/nvim-treesitter-context'
-  use 'ludovicchabant/vim-gutentags' -- Automatic tags management
+    -- Highlight, edit, and navigate code using a fast incremental parsing library
+    use 'nvim-treesitter/nvim-treesitter'
+    -- Additional textobjects for treesitter
+    use 'nvim-treesitter/nvim-treesitter-textobjects'
+    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'ludovicchabant/vim-gutentags' -- Automatic tags management
 
-
-        -- TELESCOPE
+    -- Lua
+    use {
+      "folke/twilight.nvim",
+      config = function()
+        require("twilight").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+    -- TELESCOPE
     -- UI to select things (files, grep results, open buffers...)
-  use { 'nvim-telescope/telescope.nvim',
+    use { 'nvim-telescope/telescope.nvim',
       requires = { 'nvim-lua/plenary.nvim' },
     }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { "nvim-telescope/telescope-file-browser.nvim" } -- file_browser
-  use {'cljoly/telescope-repo.nvim'}
+    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use { "nvim-telescope/telescope-file-browser.nvim" } -- file_browser
+    use {'cljoly/telescope-repo.nvim'}
+    use {'nvim-telescope/telescope-project.nvim'}
 
-  use {'ygm2/rooter.nvim'} -- change cwd to current buffer
 
-  use 'itchyny/lightline.vim' -- Fancier statusline
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'} --bufferline
- use({"gelguy/wilder.nvim" })
-  use('tpope/vim-eunuch') -- Unix commands
-  use('folke/trouble.nvim') -- quickfix list
-  use 'karb94/neoscroll.nvim' -- smooth scroll
-    -- use {
-	-- "windwp/nvim-autopairs",
-    -- config = function() require("nvim-autopairs").setup {} end
--- }
-use { 'ThePrimeagen/vim-be-good'}
-use {
-  "windwp/nvim-autopairs",
-  wants = "nvim-treesitter",
-  module = { "nvim-autopairs.completion.cmp", "nvim-autopairs" },
-  config = function()
-    require("config.autopairs").setup()
-  end,
-}
+    -- STATUSLINES
+    use 'itchyny/lightline.vim' -- Fancier statusline
+    use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'} --bufferline
 
-  use 'tpope/vim-surround' -- add )}] around
-  use 'wellle/targets.vim' -- adds more text targets (, etc
-  use 'kyazdani42/nvim-web-devicons'
+    use({"gelguy/wilder.nvim" })
+    use('tpope/vim-eunuch') -- Unix commands
+    use('folke/trouble.nvim') -- quickfix list
+    use 'karb94/neoscroll.nvim' -- smooth scroll
+    use { 'ThePrimeagen/vim-be-good'}
+
+    -- auto closing
+    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+    use 'tpope/vim-surround' -- add )}] around
+    use 'wellle/targets.vim' -- adds more text targets (, etc
+    use 'kyazdani42/nvim-web-devicons'
     -- highlight cursor on move
-  use {'edluffy/specs.nvim'}
+    use {'edluffy/specs.nvim'}
+    -- auto closing
+    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 
+    use {
+      "folke/zen-mode.nvim",
+      config = function()
+        require("zen-mode").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
 
-  -- Add indentation guides even on blank lines
-  use 'lukas-reineke/indent-blankline.nvim'
+    -- Add indentation guides even on blank lines
+    use 'lukas-reineke/indent-blankline.nvim'
 
-   -- Echoes the path to the identifier under the cursor.
-  use 'mogelbrod/vim-jsonpath'
-  -- THEMES
-  use {'sainnhe/gruvbox-material'}
-  use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
-  use 'yashguptaz/calvera-dark.nvim'
+    -- Echoes the path to the identifier under the cursor.
+    use 'mogelbrod/vim-jsonpath'
+    -- THEMES
+    use { 'folke/tokyonight.nvim'}
+    use { 'EdenEast/nightfox.nvim'}
+    use { 'Shatur/neovim-ayu'}
+    use "savq/melange"
+    use { 'NTBBloodbath/doom-one.nvim' }
+    use {'sainnhe/gruvbox-material'}
+    use {'sainnhe/everforest'}
+    use 'mjlbach/onedark.nvim' -- Theme inspired by Atom
   end,
   config = {
-       compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
+    compile_path = vim.fn.stdpath('config')..'/lua/packer_compiled.lua'
   },
 })
 
@@ -155,6 +175,8 @@ use {
 -- Clear cache
 -- :LuaCacheClear
 require'impatient'.enable_profile()
+require("trouble").setup {}
+
 -- use terminal colors
 vim.o.termguicolors = true
 vim.cmd [[colorscheme gruvbox-material]]
@@ -164,33 +186,29 @@ vim.g.lightline = {
   component_function = { gitbranch = 'fugitive#head' },
 }
 local result = vim.api.nvim_exec(
-[[
+  [[
 
-   let g:lightline.separator = { 'left': "\ue0b8", 'right': "\ue0be " }
-   let g:lightline.subseparator = { 'left': "\ue0b9", 'right': "\ue0b9" }
-   let g:lightline.tabline_separator = { 'left': "\ue0bc", 'right': "\ue0ba" }
-   let g:lightline.tabline_subseparator = { 'left': "\ue0bb", 'right': "\ue0bb" }
-   let g:tmuxline_preset = {
-        \'a'    : '#S',
-        \'b'    : '%R',
-        \'c'    : [ '#{sysstat_mem} #[fg=blue]\ufa51#{upload_speed}' ],
-        \'win'  : [ '#I', '#W' ],
-        \'cwin' : [ '#I', '#W', '#F' ],
-        \'x'    : [ "#[fg=blue]#{download_speed} \uf6d9 #{sysstat_cpu}" ],
-        \'y'    : [ '%a' ],
-        \'z'    : '#H #{prefix_highlight}'
-        \}
+  let g:lightline.separator = { 'left': "\ue0b8", 'right': "\ue0be " }
+  let g:lightline.subseparator = { 'left': "\ue0b9", 'right': "\ue0b9" }
+  let g:lightline.tabline_separator = { 'left': "\ue0bc", 'right': "\ue0ba" }
+  let g:lightline.tabline_subseparator = { 'left': "\ue0bb", 'right': "\ue0bb" }
+  let g:tmuxline_preset = {
+  \'a'    : '#S',
+  \'b'    : '%R',
+  \'c'    : [ '#{sysstat_mem} #[fg=blue]\ufa51#{upload_speed}' ],
+  \'win'  : [ '#I', '#W' ],
+  \'cwin' : [ '#I', '#W', '#F' ],
+  \'x'    : [ "#[fg=blue]#{download_speed} \uf6d9 #{sysstat_cpu}" ],
+  \'y'    : [ '%a' ],
+  \'z'    : '#H #{prefix_highlight}'
+  \}
   let g:tmuxline_separators = {
-        \ 'left' : "\ue0bc",
-        \ 'left_alt': "\ue0bd",
-        \ 'right' : "\ue0ba",
-        \ 'right_alt' : "\ue0bd",
-        \ 'space' : ' '}
+  \ 'left' : "\ue0bc",
+  \ 'left_alt': "\ue0bd",
+  \ 'right' : "\ue0ba",
+  \ 'right_alt' : "\ue0bd",
+  \ 'space' : ' '}
 ]], false
 )
 --Enable mouse mode
 vim.o.mouse = 'a'
- require("trouble").setup {}
-require'telescope'.load_extension'repo'
-
-vim.g['rooter_cd_cmd'] = 'lcd'
