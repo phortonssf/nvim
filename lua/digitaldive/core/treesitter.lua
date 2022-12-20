1f92e966fa716832ce40f22aa121e0879186a102
Peter Horton <phorton@softstackfactory.com>
2022-12-19 16:32:36 -0800

    (feat:nvim) add matchup

--- lua/digitaldive/core/treesitter.lua
+++ lua/digitaldive/core/treesitter.lua
@@ -87,4 +87,9 @@ textobjects = {
       },
     },
   },
+  matchup = {
+    enable = true,              -- mandatory, false will disable the whole extension
+    disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+    -- [options]
+  },
 }
