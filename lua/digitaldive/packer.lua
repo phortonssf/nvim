53200e1f78debe74974da30e62fabe8ba24bd03e
Peter Horton <phorton@softstackfactory.com>
2022-12-20 19:29:23 -0800

    (feat:nvim) packer updates???

--- lua/digitaldive/packer.lua
+++ lua/digitaldive/packer.lua
@@ -1,7 +1,9 @@
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
+local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  is_bootstrap = true
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
 end
 
 vim.cmd [[
