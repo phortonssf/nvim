5b1c2507ee9730ed686ec591595639c1be92fb78
Peter Horton <phorton@softstackfactory.com>
2022-12-27 17:25:00 -0800

    (feat:nvim) telescope working funcs with reload

---
+++ lua/digitaldive/telescope/mappings.lua
@@ -0,0 +1,32 @@
+
+if not pcall(require, "telescope") then
+  return
+end
+
+local sorters = require "telescope.sorters"
+
+TelescopeMapArgs = TelescopeMapArgs or {}
+
+local map_tele = function(key, f, options, buffer)
+  local map_key = vim.api.nvim_replace_termcodes(key .. f, true, true, true)
+
+  TelescopeMapArgs[map_key] = options or {}
+
+  local mode = "n"
+  local rhs = string.format("<cmd>lua R('digitaldive.telescope')['%s'](TelescopeMapArgs['%s'])<CR>", f, map_key)
+
+  local map_options = {
+    noremap = true,
+    silent = true,
+  }
+
+  if not buffer then
+    vim.api.nvim_set_keymap(mode, key, rhs, map_options)
+  else
+    vim.api.nvim_buf_set_keymap(0, mode, key, rhs, map_options)
+  end
+end
+
+
+map_tele("<space>fa", "find_project")
+map_tele("<space>fi", "search_all_files")
