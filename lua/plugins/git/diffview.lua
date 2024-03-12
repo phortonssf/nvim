f41af04af0eb9c3bdb8bbc4a28a7f2f409a4c1c3
Peter Horton <phorton@softstackfactory.com>
2024-03-12 18:51:48 +0000

    (feat/nvim) adds diffview

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,8 +1,20 @@
 return {
   "sindrets/diffview.nvim",
-  Event = "VeryLazy",
+  lazy = false,
   keys = {
-    { "<leader>gd", "<cmd>DiffviewFileHistory % <cr>", desc = "Diffview Current File" },
+    { "<leader>gd", "<cmd>DiffviewFileHistory %  --base=LOCAL <cr>", desc = "Diffview Current File" },
     { "<leader>gD", "<cmd>DiffviewFileHistory <cr> ", desc = "Diffview Current Branch" },
+    { "<leader>qd", "<cmd>DiffviewClose <cr>", desc = "Close DiffView" },
+  },
+  opts = {
+    view = {
+      file_history = {
+        winbar_info = true,
+      },
+    },
+    default_args = {
+      DiffviewOpen = { "--untracked-files=no", "--imply-local" },
+      DiffviewFileHistory = { "--base=LOCAL" },
+    },
   },
 }
